fff8d96860a4cae25ea2a571d9538e0377e3c20c
Guilherme Cardoso <guilhermecardosowork@gmail.com>
2023-01-27 21:02:04 -0300

    :tada:Deploy

---
+++ prisma/migrations/20230127234645_deploy/migration.sql
@@ -0,0 +1,17 @@
+-- DropForeignKey
+ALTER TABLE `accounts` DROP FOREIGN KEY `accounts_user_id_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `schedulings` DROP FOREIGN KEY `schedulings_user_id_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `sessions` DROP FOREIGN KEY `sessions_user_id_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `user_time_intervals` DROP FOREIGN KEY `user_time_intervals_user_id_fkey`;
+
+-- RenameIndex
+ALTER TABLE `accounts` RENAME INDEX `accounts_user_id_fkey` TO `accounts_user_id_idx`;
+
+-- RenameIndex
+ALTER TABLE `sessions` RENAME INDEX `sessions_user_id_fkey` TO `sessions_user_id_idx`;
